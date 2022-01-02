a4a0f50d4824e2200bba3e356e39537530bdb8e8
prasannaraavi <prasannaraavi@Prasannas-MBP.lan>
2022-01-02 00:06:51 -0500

    main.go

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/prasannayanamadala/assignments/variable"
+import (
+	"fmt"
+
+	"github.com/prasannaraavi/assignments/variable"
+)
 
 func main() {
 
 	variable.VarDeclaration()
+	
+	variable.ScopeDeclaration()
+	
+	variable.NextFunc()      //Functional scope
+
+	variable.Scope1Func()    //package level scope
+
+	fmt.Println("pkgvariable3:", variable.Pkgvariable3)  //global scope
+	
 
 }
