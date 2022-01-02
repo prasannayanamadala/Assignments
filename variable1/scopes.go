50cd7ae2f6d613408583c657ee1d637a22d5c898
prasannaraavi <raaviprasanna9@gmail.com>
2022-01-02 00:53:17 -0500

    scopes.go

---
+++ variable1/scopes.go
@@ -0,0 +1,11 @@
+package variable1
+
+import ( 
+	"fmt"
+	"github.com/prasannaraavi/assignments/variable"
+)
+
+func Scope2Func() {
+
+	fmt.Println("pkgvariable3:", variable.Pkgvariable3) //global scope 
+}
